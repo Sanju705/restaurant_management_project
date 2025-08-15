484b2e6d70c6dfd4b89e4ee498455adf54c7ec25
Sanju705 <kamanisanju2705@gmail.com>
2025-08-16 01:18:42 +0530

    27597

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Restaurant
 # Create your views here.
+def home(request):
+    restaurant = Restaurant.objects.first()
+    return render(request, "home.html", {"restaurant_name": Q Restaurant})
