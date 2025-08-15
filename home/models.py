64dfb70ef0afeafd932fee5bb06b7897e09e8f35
Sanju705 <kamanisanju2705@gmail.com>
2025-08-16 01:35:23 +0530

    27597

--- home/models.py
+++ home/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Restaurant(models.Model):
-    name = models.CharField(max_length=255)
+# class Restaurant(models.Model):
+#     name = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
