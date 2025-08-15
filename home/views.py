de157dedd849f0b03ab1e7bc7352f71f64e93dba
Sanju705 <kamanisanju2705@gmail.com>
2025-08-16 01:28:05 +0530

    27597

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from .models import Restaurant
 # Create your views here.
 def home(request):
     restaurant = Restaurant.objects.first()
-    return render(request, "home.html", {"restaurant_name": Q Restaurant})
+    return render(request, "home.html", {"restaurant_name": "Q Restaurant"})
