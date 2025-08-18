8c6ab03e6622c9599978b158c7495922c91eeb2f
Sanju705 <kamanisanju2705@gmail.com>
2025-08-18 23:39:38 +0530

    35544

--- home/views.py
+++ home/views.py
@@ -1,7 +1,23 @@
 from django.conf import settings
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 # from .models import Restaurant
 # Create your views here.
 def home(request):
     restaurant_name=getattr(settings, "RESTAURANT_NAME", "Our Restaurant")
     return render(request, "home.html", {"restaurant_name": Q restaurant})
+
+
+class MenuItemView(APIView):
+
+    def get(self, request):
+        menu_item = [
+            {"id": 1, "name": "pizze", "price":199},
+            {"id": 2, "name": "paneer", "price": 299},
+            {"id": 3, "name": "Burger", "price": 99},
+            {"id": 4, "name": "coffee", "price": 129},
+        ]
+        return Response (menu_item, status=status.HTTP_200_OK)
+         
