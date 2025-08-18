ba9c6316339311e4ad37b535a5eab533820af647
Sanju705 <kamanisanju2705@gmail.com>
2025-08-18 23:28:28 +0530

    35544

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import *
+from .views import MenuItemView
 
 urlpatterns = [
-    
+    path("menu-item/", MenuItemListView.as_view(), name="menu-item"),
 ]
